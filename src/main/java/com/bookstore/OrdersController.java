659fee27bda1e92f650294171a7aab9d1234a8e0
Niel Thiart <niel@thiart.com>
2024-11-08 07:43:07 +0200

    Use ArraySchema in OrdersController - this response returns an array

--- src/main/java/com/bookstore/OrdersController.java
+++ src/main/java/com/bookstore/OrdersController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -78,7 +79,7 @@ public class OrdersController {
     @Operation(summary = "List all orders", description = "Get a list of all orders in the system")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Successful operation",
-                    content = @Content(schema = @Schema(implementation = Order.class)))
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = Order.class))))
     })
     @GetMapping
     public ResponseEntity<List<Order>> listOrders() {
